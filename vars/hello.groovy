f9d5e0d7f41e7a211ddd52f97161b37492ab97f0
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-19 14:26:45 +0530

    Implement Docker login and image tagging/pushing
    Added Docker login and image push functionality.

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,7 @@
 def call(){
-  echo "hello sandesh this is your shared-lib"
+  withCredentials([usernamePassword(credentialsId:"docker-cred",passwordVariable:"DOCKER_PASS",usernameVariable:"DOCKER_USER")]){
+  sh "docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS}"
+  sh "docker image tag two-tier ${env.DOCKER_USER}/two-tier:latest"
+  sh "docker push ${env.DOCKER_USER}/two-tier:latest"
+  }  
 }
