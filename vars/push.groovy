325967e59fbf5042457121fa85a2a5e723c8d12f
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-20 11:50:15 +0530

    Fix typo in variable name for Docker username

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,5 +1,5 @@
 def call(String Project,string ImageTage,string dockerhubuser){
-  withCredentials([usernamePassword(credentialsId:"docker-cred",passwordVariable:"DOCKER_PASS",usernameVariable:"DOCKER_USE")]){
+  withCredentials([usernamePassword(credentialsId:"docker-cred",passwordVariable:"DOCKER_PASS",usernameVariable:"DOCKER_USER")]){
 }
 sh "docker push ${DOCKER_USER}/${Project}:${ImageTage}"
 }
