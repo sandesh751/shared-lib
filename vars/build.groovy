2c72b2f860dc46f4d89eb61069f07ade7f16eb51
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-20 11:44:08 +0530

    Fix typo in DockerHubUser variable name

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,3 @@
 def call(String ProjectName, String ImageTage, String DockerHubUser){
-  sh "docker build -t ${DckerHubUser}/${ProjectName}:${ImageTage} ."
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTage} ."
 }
