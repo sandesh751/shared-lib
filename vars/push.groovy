d40c7afe5813de23c7dff388ff5d0b1146ed9a13
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-21 12:01:36 +0530

    Fix parameter type from 'string' to 'String'

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,4 @@
-def call(String Project,string ImageTage,string dockerhubuser){
+def call(String Project,String ImageTage,String dockerhubuser){
   withCredentials([usernamePassword(credentialsId:"docker-cred",passwordVariable:"DOCKER_PASS",usernameVariable:"DOCKER_USER")]){
 }
 sh "docker push ${DOCKER_USER}/${Project}:${ImageTage}"
