03f5ed9c44514ee1a0168ddfff23426283954e66
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-21 12:15:49 +0530

    Update docker login command to use --password-stdin

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,6 +1,6 @@
 def call(String Project,String ImageTage,String dockerhubuser){
   withCredentials([usernamePassword(credentialsId:"docker-cred",passwordVariable:"DOCKER_PASS",usernameVariable:"DOCKER_USER")]){
-  sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}"
+  sh "docker login -u ${DOCKER_USER} --password-stdin ${DOCKER_PASS}"
 }
 sh "docker push ${DOCKER_USER}/${Project}:${ImageTage}"
 }
