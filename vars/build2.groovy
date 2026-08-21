8763b2c631e2b1426e4c47ff0878aa4484edbc55
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-21 16:36:30 +0530

    Update build2.groovy

--- vars/build2.groovy
+++ vars/build2.groovy
@@ -1,3 +1,13 @@
-def call(String projectName , String ImageTag ){
-  sh "docker build -t ${projectName}:${ImageTag} ."
+def call(String projectName, String imageTag) {
+    sh "docker build -t ${projectName}:${imageTag} ."
+    sh "trivy image --format template --template '@contrib/html.tpl' -o trivy-report.html ${projectName}:${imageTag}"
+    publishHTML([
+        allowMissing: false,
+        alwaysLinkToLastBuild: true,
+        keepAll: true,
+        reportDir: '.',
+        reportFiles: 'trivy-report.html',
+        reportName: 'Trivy Security Report' 
+    ])
 }
+
