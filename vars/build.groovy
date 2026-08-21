93b41a6187644124102dfce3cc9f3ef34739d924
sandesh751 <sandeshsaini7983637650@gmail.com>
2026-08-21 10:51:13 +0530

    Enhance call function with dockerfile and context paths

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,4 @@
-def call(String ProjectName, String ImageTage, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTage} ."
+def call(String projectName, String imageTag, String dockerHubUser, String dockerfilePath = "Dockerfile", String contextPath = ".") {
+    // -f flag se hum custom Dockerfile name paas kar sakte hain
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} -f ${dockerfilePath} ${contextPath}"
 }
